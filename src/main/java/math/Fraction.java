735ef00399bde39f0f1caf8ca5f101fe7dfc5ed5
Ferenc Barta <barta.ferenc01@gmail.com>
2014-05-22 11:13:43 +0200

    Implemented Fraction.isZero() method.

--- src/main/java/math/Fraction.java
+++ src/main/java/math/Fraction.java
@@ -226,7 +226,7 @@ public class Fraction extends Number implements Cloneable {
 	 * @return {@code true} if this fraction is equal to zero, {@code false} otherwise
 	 */
 	public boolean isZero() {
-		// TODO
+		if (numerator == 0) return true;
 		return false;
 	}
 
